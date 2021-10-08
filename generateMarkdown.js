34df9acf01c8b4cbae9c84232d5935386e53e604
GriffinWilkens96 <gwilkens1234@gmail.com>
2021-10-07 22:42:47 -0400

    render license update

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -29,20 +29,32 @@ function renderLicenseLink(license) {
       return 'https://opensource.org/licenses/MIT';
     case '':
       break;
-}
+  }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  let licenseBadge = renderLicenseBadge(data.licenses) 
-
+  switch(license) {
+    case 'Apache 2.0':
+      return 'Apache 2.0';
+    case 'BSD 3':
+      return 'BSD 3';
+    case 'GPL 3.0':
+      return 'GPL 3.0';
+    case 'MIT':
+      return 'MIT';
+    case '':
+      break;
+  }
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  let licenseBadge = renderLicenseBadge(data.licenses);
+  let licenseLink = renderLicenseLink(data.licenses);
   return `# ${data.title}
-  [![License: ${data.licenses}] [${licenseBadge}]
+  [![License: ${licenseLink}] [${licenseBadge}]
 `;
 }
 
